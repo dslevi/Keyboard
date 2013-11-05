eb08176e158da51f04eb4a893cb8ca9851791c9d
Hackbright Student <user@chromebox08.(none)>
2013-11-04 18:30:19 -0800

    Revamped classes

--- webapp.py
+++ webapp.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("keypress_v2.html")
+    return render_template("keypress_v3.html")
 
 @app.route("/bigrams", methods=["POST"])
 def show_bigrams():
-    freq = request.form.get("freq")
-    count = request.form.get("count")
+    text = request.form.get("text")
+    raw_text = request.form.get("rawtext")
     strokes = request.form.get("stroke")
     timestamps = model.get_timestamps(strokes)
     bigrams = model.find_bigrams(timestamps)
-    return render_template("bigrams.html", freq=freq, count=count, strokes=strokes, 
-                            timestamps=timestamps, bigrams=bigrams)
+    return render_template("bigrams.html", strokes=strokes, 
+                            timestamps=timestamps, bigrams=bigrams, text=text, raw_text=raw_text)
 
 
 if __name__ == "__main__":
